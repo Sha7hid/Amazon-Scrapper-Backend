a043fe4a0e788d18513b37fc2b7a8f76a8504dd1
sha7hid <shahidshafadshahad@gmail.com>
2023-10-29 12:16:09 +0530

    updt

--- inngest/client.js
+++ inngest/client.js
@@ -5,5 +5,5 @@ const { Inngest } = require("inngest");
 // Create a client to send and receive events
 const inngest = new Inngest({ id: "my-app" ,eventKey:process.env.INNGEST_EVENT_KEY});
 console.log(process.env.INNGEST_EVENT_KEY)
-inngest.setEventKey('iUkU9GwsxJL2D5VbY444cz0DE95M-K-7Hox6aVcenM0LPSObgA7V3kN0qQNzwXyaTqfzrEusNgjYu3AZWeBzZw')
+inngest.setEventKey('yA46r6OmwIn4u6iInLWNQM_Bv3azjq1mzKvmTwrjsQ-hqVs6xzjSg5_I_WjlhkNmmpk8wflIH2O2B9hsd9insw')
 module.exports = {inngest};
